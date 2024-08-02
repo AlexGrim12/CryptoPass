1f81e81ad7699a1be33785336ceb618e7ad07100
AlexGrim <alejandrogrim12@gmail.com>
2024-08-01 19:26:54 -0600

    chore: Update NavbarComponent to include logo

--- app/components/NavbarComponent.tsx
+++ app/components/NavbarComponent.tsx
@@ -2,12 +2,16 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import Cart from '../assets/cart.png'
+import Logo from '../assets/logo.png'
 
 export default function Navbar() {
   return (
     <header className="flex justify-between items-center p-6 bg-zinc-800 bg-opacity-5 backdrop-blur-sm shadow-lg">
       <Link href="/" className="text-2xl font-bold">
-        NFT Ticket Store
+        <Image src={Logo} alt="Logo" width={40} height={40} />
+        <p className="text-sm font-bold text-white cursor-pointer">
+          NFT Ticket Store
+        </p>
       </Link>
       <nav>
         {/* events, checkout */}
